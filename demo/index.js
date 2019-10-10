299b2bc4711e4938c6c8f7011793719ee8b44e78
徐书迪 <deantsui0xff@gmail.com>
2019-10-10 15:33:36 +0800

    力图

--- demo/index.js
+++ demo/index.js
@@ -2,59 +2,150 @@ import TopoFlow from '../lib';
 // import TopoFlow from '../src';
 import './index.scss'
 
+import testSvg from './test.svg'
+const json = `    
+{    "jumpCoreInfo": {
+        "links": [
+            {
+                "from": "235834",
+                "id": null,
+                "to": "235371"
+            },
+            {
+                "from": "235371",
+                "id": "13054",
+                "to": "235390"
+            },
+            {
+                "from": "235390",
+                "id": "13055",
+                "to": "235379"
+            },
+            {
+                "from": "235379",
+                "id": "13056",
+                "to": "235378"
+            },
+            {
+                "from": "235378",
+                "id": null,
+                "to": "235835"
+            },
+            {
+                "from": "235839",
+                "id": null,
+                "to": "235378"
+            },
+            {
+                "from": "235378",
+                "id": "13056",
+                "to": "235379"
+            },
+            {
+                "from": "235379",
+                "id": "13057",
+                "to": "235382"
+            },
+            {
+                "from": "235382",
+                "id": null,
+                "to": "235840"
+            },
+            {
+                "from": "235382",
+                "id": "13058",
+                "to": "235386"
+            },
+            {
+                "from": "235386",
+                "id": "13059",
+                "to": "235389"
+            },
+            {
+                "from": "235389",
+                "id": null,
+                "to": "235841"
+            }
+        ],
+        "nodes": [
+            {
+                "name": "阜新市阜新银行总行资源点机房ODF",
+                "id": "235834",
+                "type": "10"
+            },
+            {
+                "name": "阜新市阜新银行总行ODF1",
+                "id": "235371",
+                "type": "2"
+            },
+            {
+                "name": "阜新市解放大街与街心路2号光交箱",
+                "id": "235390",
+                "type": "1"
+            },
+            {
+                "name": "阜新经济开发区5号光交箱",
+                "id": "235379",
+                "type": "1"
+            },
+            {
+                "name": "阜新市阜新银行液压支行资源ODF1",
+                "id": "235378",
+                "type": "2"
+            },
+            {
+                "name": "阜新市阜新银行液压支行资源点机房ODF",
+                "id": "235835",
+                "type": "10"
+            },
+            {
+                "name": "910F-阜新液压支行",
+                "id": "235839",
+                "type": "10"
+            },
+            {
+                "name": "阜新市中天机房ODF1",
+                "id": "235382",
+                "type": "2"
+            },
+            {
+                "name": "960-阜新中天公司无线机房",
+                "id": "235840",
+                "type": "10"
+            },
+            {
+                "name": "阜新市开发区无线机房ODF2",
+                "id": "235386",
+                "type": "2"
+            },
+            {
+                "name": "阜新喀斯汀无线机房ODF1",
+                "id": "235389",
+                "type": "2"
+            },
+            {
+                "name": "960-阜新喀斯汀无线机房",
+                "id": "235841",
+                "type": "10"
+            }
+        ]
+    }}`
 class Index {
     constructor() {
-        this.data = {                      // 默认图形数据
-            nodes: [{
-                id: 'router1',
-                type: 'router',
-                x: 300,
-                y: 150,
-                name: "router1"
-                // config: {  }
-            }, {
-                id: 'switch2',
-                type: 'switch',
-                x: 300,
-                y: 300,
-                name: "switch2"
-                // config: {  }
-            }, {
-                id: 'server1',
-                type: 'server',
-                x: 500,
-                y: 450,
-                name: "server1",
-                // config: {  }
-            }, {
-                id: 'server2',
-                type: 'server',
-                x: 200,
-                y: 450,
-                name: "server2"
-                // config: {  }
-            }],
-            links: [{
-                from: 'router1',
-                to: 'switch2',
-                // id:'1'
-            }, {
-                from: 'server1',
-                to: 'switch2',
-                id:'2'
-            }, {
-                from: 'server2',
-                to: 'switch2',
-                // id:'3'
-            }]
-        }
+        this.data = JSON.parse(json).jumpCoreInfo
     }
 
     initTopoFlow() {
+
+        this.data.nodes.forEach(element => {
+                element['x'] =0;
+                element['y'] =0;
+        });
         let config = {
             eln: '#topoflow1',
             data: this.data,
-            height: `700px`,
+            height: `600px`,
+            width: `1200px`,
             // readOnly:true,
             // 模板
             linkTemplate:{
@@ -64,60 +155,57 @@ class Index {
                 }
             },
             nodeTemplate: {
-                router: {
-                    width: 90,
+                "1": {
+                    width: 50,
                     height: 50,
                     deleteAble: true,
                     operatingPoint: ['left', 'right'],
                     renderNode: (node, nodeInfo) => {
-                        node.append('svg:rect')
-                            .style('fill', 'white')
-                            .style('stroke', '#27af7d')
+                        node.append('image')
+                            .attr('xlink:href',testSvg)
                             .attr('height', nodeInfo.height)
                             .attr('width', nodeInfo.width)
                         node.append('text')
-                            .attr('x', 10)
-                            .attr('y', 40)
+                            .attr('x', -30)
+                            .attr('y', 70)
                             .html(nodeInfo.name);
                     }
                 },
-                switch: {
-                    width: 140,
+                "10": {
+                    width: 50,
                     height: 50,
                     deleteAble: true,
                     operatingPoint: [
                         'left', 'right'
                     ],
                     renderNode: (node, nodeInfo) => {
-                        node.append('svg:rect')
-                            .style('fill', 'white')
-                            .style('stroke', '#27af7d')
-                            .attr('height', nodeInfo.height)
-                            .attr('width', nodeInfo.width)
+                        node.append('image')
+                        .attr('xlink:href',testSvg)
+                        .attr('height', nodeInfo.height)
+                        .attr('width', nodeInfo.width)
 
                         node.append('text')
-                            .attr('x', 10)
-                            .attr('y', 40)
+                        .attr('x', -30)
+                        .attr('y', 70)
                             .html(nodeInfo.name);
                     }
                 },
-                server: {
+                "2": {
                     width: 50,
-                    height: 100,
+                    height: 50,
                     deleteAble: true,
                     operatingPoint: [
                         'left', 'right'
                     ],
                     renderNode: (node, nodeInfo) => {
-                        node.append('svg:rect')
-                            .style('fill', 'white')
-                            .style('stroke', '#27af7d')
-                            .attr('height', nodeInfo.height)
-                            .attr('width', nodeInfo.width)
+                        node.append('image')
+                        .attr('xlink:href',testSvg)
+                        .attr('height', nodeInfo.height)
+                        .attr('width', nodeInfo.width)
 
                         node.append('text')
-                            .attr('x', 10)
-                            .attr('y', 40)
+                        .attr('x', -30)
+                        .attr('y', 70)
                             .html(nodeInfo.name);
                     }
                 }
@@ -173,10 +261,10 @@ class Index {
             let nodeType = document.querySelector('#node-type').value;
             let name = document.querySelector('#node-name').value || `${nodeType}_${Math.floor(Math.random() * 1000)}`;
             this.topoFlow.addNode({
-                type: nodeType,
+                type: '1',
                 x: 100,
                 y: 100,
-                config: { name }
+                name 
             });
         });
 
@@ -184,17 +272,17 @@ class Index {
         document.querySelector('#btn2').addEventListener('click', () => {
 
             let node1 = this.topoFlow.addNode({
-                type: 'router',
+                type: '1',
                 x: 100,
                 y: 100,
-                config: { name: 'node1' }
+                name 
             });
 
             let node2 = this.topoFlow.addNode({
-                type: 'router',
+                type: '1',
                 x: 400,
                 y: 100,
-                config: { name: 'node2' }
+                name 
             });
             this.topoFlow.addLink(node1, node2)
         });
